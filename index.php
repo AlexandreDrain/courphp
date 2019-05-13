acdb0971a9a601a032b92b960184a73262a8c914
Alexandre <alexendredrain@gmail.com>
2019-05-13 15:08:55 +0200

    Avancé dans le code nous en avons fini les conditions

--- index.php
+++ index.php
@@ -14,9 +14,118 @@
 <?php
 
     echo 'Bonjour';
-    
-    ?>
+    $bl1 = 1.55;
+    $isPublished = true;
+
+    $content = "<p>Contenu de mon paragraphe : $bl1</p>"; // interprete du code
+    $content2 = '<p>Contenu de mon paragraphe : $bl1</p>'; // n'interprete pas
+
+    echo $content;
+    echo $content2;
+
+    // Les constantes
+    const NB_ARTICLE_PER_PAGE = 6;
+
+    echo NB_ARTICLE_PER_PAGE;
+
+    /*On ne modifie pas une constante
+     NB_ARTICLE_PER_PAGE = 12; */
+
+    echo "<br>";
+    echo __LINE__;
+    echo "<br>";
+
+    $cpt = 0;
+    $cpt++;
+    echo $cpt;
+    echo "<br>";
+    echo "compteur avant :" . ++$cpt;
+    echo "<br>";
+
+    $nb = 1;
+    $result = (1 == $nb);
+    echo "Résultat : " . $result . "|";
+    var_dump($result);
+
+
+    $lastName = "Alex";
+    $name = $firstName ?? $lastName;
+    echo $name;
+
+    $test = true;
+    echo !$test;
+
+    $result2 = ($test === true) ? 'oui' : 'non';
+
+    /*if($test === true) {
+        $result2 = 'oui';
+    } else {
+        $result2 = 'non'
+    }*/ // Cette condition revient à faire la même chase que la ligne du dessus.
+
+    $day = "vendredi";
+    if($day === "samedi" || $day === "dimanche") {
+        echo "<p>C'est le week-end !</p>";
+    }elseif ($day === "vendredi") {
+        echo "<p>C'est bientôt le week-end!</p>";
+    }else {
+        echo "<p>Ce n'est pas le week-end...</p>";
+    }
+
+    //---------------------EXERCICE--------------------------
+
+    $temp = 20;
+    if ($temp < 0) {
+        echo "<p>Il fait vraiment super froid...</p>";
+    }elseif ($temp < 20) {
+        echo "<p>Il fait frais sortez les gilets</p>";
+    }elseif ($temp < 50){
+        echo "<p>Il fait chaud, presque la canicule !</p>";
+    }else {
+        echo "<p>On brûle!!</p>";
+    }
+
+    $day2 = "lundi";
+
+    switch($day2){
+
+        case 'vendredri' :
+            echo 'c\'est presque le week-end';
+            break;
+        case 'samedi' :
+            echo 'Nous sommes le wekk-end';
+            break;
+        case 'dimanche' :
+            echo 'Nous sommes le week-end';
+            break;
+        default :
+            echo 'Nous ne sommes pas le week-end';
+            break;
+    }
+
+?>
 
 
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
