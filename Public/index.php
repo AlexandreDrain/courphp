6b3829b180c91a6821d63c76e598f3b9340b6cdf
Alexandre <alexendredrain@gmail.com>
2019-05-14 11:28:41 +0200

    fichier header/footer.php plus includ

--- Public/index.php
+++ Public/index.php
@@ -1,8 +1,9 @@
 <?php
 
-    include ("header.php");
+    require("header.php");
 
 ?>
+<h1 class="text-white bg-dark">Ma page</h1>
 
 <?php
 
@@ -129,7 +130,6 @@
     ];
 
 
-
 ?>
 
 <table>
@@ -157,12 +157,9 @@
     </tbody>
 </table>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>
-</html>
-
+<?php
+require("footer.php");
+?>
 
 
 
---
+++ Public/tableau.php
@@ -0,0 +1,17 @@
+<?php
+require("header.php");
+?>
+
+<h1> Les tableaux </h1>
+
+<?php
+
+
+
+?>
+
+
+
+<?php
+    require("footer.php");
+?>
