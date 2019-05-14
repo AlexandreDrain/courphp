5e6ebbc1447853ecd1db943011b0e7f0463a6027
Alexandre <alexendredrain@gmail.com>
2019-05-14 11:01:45 +0200

    include

--- index.php
+++ index.php
@@ -1,15 +1,8 @@
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Cour PHP</title>
-</head>
-<body>
+<?php
 
-<h1>Ma page</h1>
+    include ("header.php");
+
+?>
 
 <?php
 
@@ -103,9 +96,70 @@
             break;
     }
 
+    // Les boucles
+    // Objectif : créer une liste à puces (10 puces)
+
+    echo "<ul>";
+    $i = 0;
+    while($i<=10) {
+        echo "<li>Puce $i</li>";
+        $i++;
+    }
+    echo "</ul>";
+
+    /*echo "<ul>"; // pareil que le while
+    for($i = 0; $i < 10; $i++){
+        echo "<li>Puce $i</li>";
+    }
+    echo "</ul>";*/
+
+    // tableau indexé.
+    $fruits = ["kiwi", "pamplemousse", "Citron", "Ananas", "orange", "Banane", "Fruit de la passion", "Litchi"];
+    echo "<ul>";
+    foreach($fruits as $clef => $fruit) { // si on veut supprimer des fruits utiliser un if dans la boucle.
+        echo "<li>$fruit</li>";
+    }
+    echo "</ul>";
+
+    // tableau associatif.
+    $people = [
+            "firsName" => "John",
+            "lastName" => "Doe",
+            "age" => "34"
+    ];
+
+
+
 ?>
 
+<table>
+    <thead>
+    <tr>
+        <th></th>
+        <?php
+            foreach ($people as $key => $value) {
+                echo "<td>$key</td>";
+            }
+        ?>
+        <th></th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <th></th>
+        <?php
+            foreach ($people as $value) {
+                echo "<td>$value</td>";
+            }
+        ?>
+        <th></th>
+    </tr>
+    </tbody>
+</table>
 
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
 
