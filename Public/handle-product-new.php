5f1f624673662fcdf71ede28d26a812ccbb5be6f
Alexandre <alexendredrain@gmail.com>
2019-05-15 16:58:33 +0200

    sécurisation du form terminé

--- Public/handle-product-new.php
+++ Public/handle-product-new.php
@@ -1,13 +1,13 @@
 <?php
 // var_dump($_GET);
- var_dump($_POST);
+// var_dump($_POST);
 
 // On vérifie que le formulaire a bien été soumis
 // $_POST : les variables du formulaires (attribut HTML name)
 // empty() : permet de tester "est-ce que c'est vide ?"
 // empty($_POST) : si $_POST est vide
 //
-if (!empty($_POST)){
+if ($_SERVER["REQUEST_METHOD"] === "POST"){
 
 //// Test du nom,
 //// L'existence
@@ -64,27 +64,50 @@ if (!array_key_exists("publie", $_POST)) {
     // syntaxe alternative
     // $_POST["publie"] = (array_key_exists("publie"), $_POST);
 
-var_dump($_POST);
+ // var_dump($_POST);
 
 if (!array_key_exists('date', $_POST)) {
     var_dump("La date de création de la date n'existe pas!");
 } elseif ($_POST['date'] === '') { //// La non-nullité
     var_dump("Il faut saisir une date de création");
 }else {
-    if (strlen($_POST["date"]) === 10 ){
-        //On décompose la date de création en trois parties
-        $tabdate = explode("-", $_POST["date"]);
+    //On décompose la date de création en trois parties
+    $tabdate = explode("-", $_POST["date"]);
+    // On vérifie qu'on est bien les 3 composantes de la date
+    if (sizeof($tabdate) === 3){
+
         //On check si la date est correcte
-        if (!checkdate($tabdate[1], $tabdate[2], $tabdate[0])) {
+    if (!checkdate($tabdate[1], $tabdate[2], $tabdate[0])) {
             var_dump("Il faut saisir une date valide");
-        }
+    }
     } else {
             var_dump("Il faut saisir une date valide");
         }
 
 }
 
-
+if (!array_key_exists('view', $_POST)) {
+    var_dump("Le nombre de vue n'existe pas !");
+} else {
+    //// La non-nullité
+    if ($_POST['view'] === '') {
+        var_dump("Il faut saisir un nombre de vue !");
+    } else {
+        //// Le type
+        if (!is_numeric($_POST['view'])) {
+            var_dump("Veuillez saisir un nombre de vue correct !");
+        }
+        $_POST['view'] = intval($_POST['view']);
+        //// Valeur mini (0)
+        if ($_POST['view'] < 0) {
+            var_dump("Le nombre de vue doit être positif !");
+        }
+        //// Valeur maxi
+        if($_POST['view'] > 19E19) {
+            var_dump("Le nombre de vue doit être inférieur a 19e19 vues !");
+        }
+    }
+}
 
 
 /*$tab = ["banane", "pamplemousse", "kiwi"];
@@ -93,6 +116,3 @@ $tab = explode(", ", $str);
  exemple */
 
 }
-
-
-
--- Public/productnew.php
+++ Public/productnew.php
@@ -8,23 +8,24 @@
 
     <h1>Ajout d'un produit</h1>
 
-    <form method="post" ><!-- class="was-validated" -->
+    <form method="post" class="was-validated" ><!-- -->
 
         <label for="name">Nom</label>
-        <input type="text" id="name" name="name" class="form-control" placeholder="Required LastName">
+        <input type="text" id="name" name="name" class="form-control" placeholder="Required LastName" required>
         <label for="firstname">Pr&eacute;nom</label>
-        <input type="text" id="firstname" name="firsname" class="form-control" placeholder="Required FirstName">
-        <label for="description">Descritpion du produit</label>
-        <textarea name="description" cols="30" rows="10" id="description" class="form-control"></textarea>
+        <input type="text" id="firstname" name="firsname" class="form-control" placeholder="Required FirstName" required>
+        <label for="description">Description du produit</label>
+        <textarea name="description" cols="30" rows="10" id="description" class="form-control" required></textarea>
         <label for="prix">Prix</label>
-        <input type="number" id="prix" name="prix" class="form-control" step="1.123456789">
+        <input type="number" id="prix" name="prix" class="form-control" step="0.01" min="0" max="10E7" required>
         <div class="custom-control custom-switch">
             <input type="checkbox" class="custom-control-input" id="publie" name="publie">
             <label class="custom-control-label" for="publie">Publish?</label>
         </div>
+        <label for="view">Nombre de vues</label>
+        <input type="number" id="view" name="view" class="form-control" step="1" required>
         <label for="date">Date</label>
-        <input type="date" id="date" name="date" class="form-control">
-
+        <input type="date" id="date" name="date" class="form-control" required>
 
         <div class="col-auto">
             <button type="submit" class="btn btn-primary mb-2">Valider</button>
